a330f95e5e5243aec311e6f80e814981951dc733
Eddysam2000 <samueledwin67@gmail.com>
2023-10-31 21:09:39 +0100

    testimonials updated

--- src/components/testimonials/Testimonial.jsx
+++ src/components/testimonials/Testimonial.jsx
@@ -5,6 +5,30 @@ import IMG2 from '../../assets/client2.webp'
 import IMG3 from '../../assets/client3.jpeg'
 import IMG4 from '../../assets/client4.jpeg'
 
+
+const data = [
+  {
+    image: IMG1,
+    name: 'BLARD Coach',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus sint officiis libero suscipit. Omnis placeat, architecto facere repudiandae tempore aspernatur culpa optio rem error officia a unde. Molestias, pariatur fugiat.'
+  },
+  {
+    image: IMG2,
+    name: 'Nathan Yinka',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus sint officiis libero suscipit. Omnis placeat, architecto facere repudiandae tempore aspernatur culpa optio rem error officia a unde. Molestias, pariatur fugiat.'
+  },
+  {
+    image: IMG3,
+    name: 'Jerome Darrell',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus sint officiis libero suscipit. Omnis placeat, architecto facere repudiandae tempore aspernatur culpa optio rem error officia a unde. Molestias, pariatur fugiat.'
+  },
+  {
+    image: IMG4,
+    name: 'Gerard Butler',
+    review: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus sint officiis libero suscipit. Omnis placeat, architecto facere repudiandae tempore aspernatur culpa optio rem error officia a unde. Molestias, pariatur fugiat.'
+  }
+]
+
 const Testimonial = () => {
   return (
     <section id='testimonials'>
@@ -12,13 +36,19 @@ const Testimonial = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials_container">
-        <article className="testimonial">
-          <div className="client_avatar">
-            <img src={IMG1} alt="Image 1" />
-            <h5 className='client_name'>Joshua Samuel</h5>
-            <small className='client_review'></small>
-          </div>
-        </article>
+        {
+          data.map(({image, name, review}, index) => {
+            return (
+              <article key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={image} />
+              </div>
+              <h5 className='client_name'>{name}</h5>
+              <small className='client_review'>{review}</small>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
