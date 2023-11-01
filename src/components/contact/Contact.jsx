6a857e1b95ec463f7fedca8a38dfa256276378f3
Eddysam2000 <samueledwin67@gmail.com>
2023-11-01 07:32:25 +0100

    contact added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,23 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className='contact_option'>
+            <h4>Email</h4>
+            <h5>samueledwin67@gmail.com</h5>
+            <a href="mailto:samueledwin67@gmail.com">Send a message</a>
+          </article>
+
+        </div>
+        {/* END OF CONTACT */}
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
