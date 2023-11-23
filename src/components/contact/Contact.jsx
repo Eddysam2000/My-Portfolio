74a480f6813522d8bd8e1bc7485bc42f6cde0e6f
Eddysam2000 <samueledwin67@gmail.com>
2023-11-23 10:13:44 +0100

    about updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
           </article>
           <article className='contact_option'>
             <BsWhatsapp className='contact_option-icon'/>
-            <h4>Whatsapp</h4>
+            <h4>WhatsApp</h4>
             <h5>+2349160399839</h5>
             <a href="https://api.whatsapp.com/send?phone+2347087743041" target='_blank'>Send a message</a>
           </article>
