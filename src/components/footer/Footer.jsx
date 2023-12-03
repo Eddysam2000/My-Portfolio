06e26b4675a11c6cef02290475a412cd94bc3d53
Eddysam2000 <samueledwin67@gmail.com>
2023-12-03 22:19:24 +0100

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="#about">Abouts</a></li>
+        <li><a href="#about">About</a></li>
         <li><a href="#experience">Experiences</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#portfolio">Portfolios</a></li>
