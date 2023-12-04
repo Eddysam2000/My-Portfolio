77aa4e62ccfc8e4df776d46463d5bf35d7145f65
Eddysam2000 <samueledwin67@gmail.com>
2023-12-04 23:57:37 +0100

    footer done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -57,7 +57,7 @@ const data = [
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      <h5>My Recent Works</h5>
+      <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio_container">
