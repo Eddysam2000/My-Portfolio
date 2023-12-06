d03d950e40bf9cf470b668fcdcee4740e831d86e
Eddysam2000 <samueledwin67@gmail.com>
2023-12-06 22:28:44 +0100

    services updated

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -107,7 +107,7 @@ const Services = () => {
             </li>
             <li>
               <BiCheck className='service_list-icon'/>
-              <p>Lorem ipsum dolor sit amet consectetur.</p>
+              <p>Lorem ipsum dolor sit amet consec.</p>
             </li>
           </ul>
         </article>
